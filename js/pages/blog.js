de97c5e7f0a75ad74c174b534deb8f0fb8b5a850
AndreasAUG23F <andabr00198@stud.noroff.no>
2024-05-08 23:24:30 +0200

    General changes

--- js/pages/blog.js
+++ js/pages/blog.js
@@ -1,35 +1,42 @@
-import { doFetch } from "../components/fetch.js";
 
-const runPage = async () => {
-    makePage();
-};
+    import { doFetch } from "../components/fetch.js";
 
-const makePage = async () => {
-    const id = window.location.search.slice(1)
-    let blog = await doFetch(
-        "GET",
-        "https://v2.api.noroff.dev/blog/posts/Swagdaddy/" + id
-    );
-    console.log("blogsInfo", blog);
+    const runPage = async () => {
+        makePage();
+    };
+    
+    const makePage = async () => {
+        const id = window.location.search.slice(1);
+        let blog = await doFetch(
+            "GET",
+            "https://v2.api.noroff.dev/blog/posts/Swagdaddy/" + id
+        );
+        console.log("blogsInfo", blog);
+    
+        let title = document.getElementById("blogHeader");
+        title.innerText = blog.title;
+    
+        let author = document.getElementById("author");
+        author.innerText = blog.author.name;
+    
+        let date = document.getElementById("updated");
+        date.innerText = blog.updated;
+    
+        let content = document.getElementById("body");
+        content.innerHTML = blog.body;
+    
+        
+        setBlogBackground(blog.media.url);
+    };
+    
+    
+    const setBlogBackground = (imageUrl) => {
+        const blogContainer = document.getElementById('blogContainer');
+        blogContainer.style.backgroundImage = `url(${imageUrl})`;
+        blogContainer.style.backdropFilter = 'blur(5px)';
 
-    let title = document.getElementById("blogHeader");
-    title.innerText = blog.title;
-
-    let author = document.getElementById("author");
-    author.innerText = blog.author.name;
-
-    let date = document.getElementById("updated");
-    date.innerText = blog.updated;
-
-    let content = document.getElementById("body");
-    content.innerHTML = blog.body;
-}
-
-
-
-
-
-
-
+    };
+    
     runPage();
+    
     
