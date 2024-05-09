8a0ca455d7e0ffe169bdb4addf19dce3f71c9320
AndreasAUG23F <andabr00198@stud.noroff.no>
2024-05-09 18:04:07 +0200

    push

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,74 +1,85 @@
-import { doFetch } from "../components/fetch.js";
+/* import { doFetch } from "../components/fetch.js";
+
+
 const runPage = async () => {
   let blogs = await doFetch(
     "GET",
     "https://v2.api.noroff.dev/blog/posts/Swagdaddy"
-  );
-  console.log("blogsInfo", blogs);
-  carousel();
-  blogGrid(blogs);
-};
-/* const carousel = () => {
-  let slideIndex = 1;
-  showSlides();
+    );
+    console.log("blogsInfo", blogs);
+    carousel(blogs);
+    blogGrid(blogs);
+  };
   
-  function showSlides() {
-    let i;
-    const slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+
+
+    const carousel = (blogs) => {
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        const slidesContainer = document.getElementById("slideshow-container");
+        slidesContainer.innerHTML = ""; 
+
+        
+        const prevButton = document.createElement("a");
+        prevButton.classList.add("prev");
+        prevButton.textContent = "❮";
+        prevButton.onclick = () => plusSlides(-1);
+
+        const nextButton = document.createElement("a");
+        nextButton.classList.add("next");
+        nextButton.textContent = "❯";
+        nextButton.onclick = () => plusSlides(1);
+
+        const readMoreButton = document.createElement("button");
+        readMoreButton.textContent = "Read More";
+        readMoreButton.classList.add("read-more-a", "carouselButton");
+
+
+        blogs.slice(-3).forEach((blog, index) => {
+            const slide = document.createElement("div");
+            slide.classList.add("mySlides");
+
+            const image = document.createElement("img");
+            image.src = blog.media.url;
+            image.alt = blog.title;
+
+            slide.appendChild(image);
+            slidesContainer.appendChild(slide);
+        });
+
+
+        slidesContainer.append(prevButton, nextButton, readMoreButton);
+
+        const slides = document.getElementsByClassName("mySlides");
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = "block";
     }
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
+
+    function plusSlides(n) {
+        showSlides((slideIndex += n));
     }
-    slides[slideIndex - 1].style.display = "block";
-  }
-  
-  function plusSlides(n) {
-    showSlides((slideIndex += n));
-  }
-  
-  document.querySelector(".next").addEventListener("click", function () {
-    plusSlides(1);
-  });
-  
-  document.querySelector(".prev").addEventListener("click", function () {
-    plusSlides(-1);
-  });
-  
-  // Legg til automatisk bildebytte hvert 3. sekund (3000 ms)
-  setInterval(function () {
-    plusSlides(1);
-  }, 3000);
 
-} */
+    document.addEventListener("keydown", function(event) {
+        if (event.keyCode === 37) {
+            plusSlides(-1);
+        } else if (event.keyCode === 39) {
+            plusSlides(1);
+        }
+    });
+};  
 
 
-const carousel = async () => {
-  // Hent de siste 3 bloggpostene fra API-et
-  const response = await doFetch("GET", "https://v2.api.noroff.dev/blog/posts");
-  const blogs = await response.json();
-  const latestBlogs = blogs.slice(-3); // Velg de siste 3 bloggpostene
 
-  // Oppdater karusellen med bildene og informasjonen om de siste 3 bloggpostene
-  latestBlogs.forEach((blog, index) => {
-    const slide = document.querySelector(`.mySlides:nth-child(${index + 1})`);
 
-    // Oppdater bilde
-    const image = slide.querySelector("img");
-    image.src = blog.image; // Bytt ut med riktig kilde fra API-et
-    image.alt = blog.title; // Sett riktig alternativ tekst fra API-et
-
-    // Oppdater tittel
-    const title = slide.querySelector("h2");
-    title.textContent = blog.title; // Sett riktig tittel fra API-et
-
-    // Vis slide
-    slide.style.display = "block";
-  });
-};
-
-carousel(); // Kjør karusellen når siden lastes
 
 
 
@@ -122,6 +133,149 @@ const blogGrid = (blogs) => {
   });
 
   return container;
+}
+
+
+
+runPage();
+carousel(); 
+ */
+
+import { doFetch } from "../components/fetch.js";
+
+
+const runPage = async () => {
+  let blogs = await doFetch(
+    "GET",
+    "https://v2.api.noroff.dev/blog/posts/Swagdaddy"
+    );
+    console.log("blogsInfo", blogs);
+    carousel(blogs); 
+    blogGrid(blogs);
+  };
+  
+  
+  
+  const carousel = (blogs) => {
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        const slidesContainer = document.getElementById("slideshow-container");
+        /* slidesContainer.innerHTML = "";  */
+
+        
+        const prevButton = document.createElement("a");
+        prevButton.classList.add("prev");
+        prevButton.textContent = "❮";
+        prevButton.onclick = () => plusSlides(-1);
+
+        const nextButton = document.createElement("a");
+        nextButton.classList.add("next");
+        nextButton.textContent = "❯";
+        nextButton.onclick = () => plusSlides(1);
+
+        const readMoreButton = document.createElement("button");
+        readMoreButton.textContent = "Read More";
+        readMoreButton.classList.add("read-more-a", "carouselButton");
+
+
+        blogs.slice(-3).forEach((blog, index) => {
+            const slide = document.createElement("div");
+            slide.classList.add("mySlides");
+
+            const image = document.createElement("img");
+            image.src = blog.media.url;
+            image.alt = blog.title;
+
+            slide.appendChild(image);
+            slidesContainer.appendChild(slide);
+        });
+
+
+        slidesContainer.append(prevButton, nextButton, readMoreButton);
+
+        const slides = document.getElementsByClassName("mySlides");
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = "block";
+    }
+
+    function plusSlides(n) {
+        showSlides((slideIndex += n));
+    }
+
+    document.addEventListener("keydown", function(event) {
+        if (event.keyCode === 37) {
+            plusSlides(-1);
+        } else if (event.keyCode === 39) {
+            plusSlides(1);
+        }
+    });
+};  
+
+
+
+
+
+
+
+
+
+const blogGrid = (blogs) => {
+  let section = document.getElementById('blogSection');
+
+  blogs.forEach(blog => {
+    let container = document.createElement('div');
+    container.classList.add('blogContainer');
+    container.id = 'blogContainer';
+
+    let imageContainer = document.createElement('div');
+    imageContainer.classList.add('blog-image');
+
+    let image = document.createElement('img');
+    image.src = blog.media.url;
+    image.alt = blog.media.alt;
+
+    let titleContainer = document.createElement('div');
+    titleContainer.classList.add('blog-title');
+
+    let title = document.createElement('h2');
+    title.classList.add('blogCardTitle');
+    title.textContent = blog.title;
+
+    let buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('readMoreButton');
+
+    let button = document.createElement('button');
+    button.textContent = 'Read More';
+    button.classList.add('read-more-a');
+
+    button.addEventListener('click', () => {
+      window.location.href = `/post/index.html?`+ blog.id;
+    });
+
+    buttonContainer.appendChild(button);
+    titleContainer.appendChild(title);
+    imageContainer.appendChild(image);
+
+    container.appendChild(imageContainer);
+    container.appendChild(titleContainer);
+    container.appendChild(buttonContainer);
+
+    section.appendChild(container);
+
+
+
+  });
+
+  return section;
 }
 
 
