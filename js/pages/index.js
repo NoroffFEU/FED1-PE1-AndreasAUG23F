6456402fbb3e40a06ec69cf4d3fe1802f4d6fa12
AndreasAUG23F <andabr00198@stud.noroff.no>
2024-05-23 14:06:59 +0200

    Update index.js

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,104 +1,95 @@
-
 import { carouselFunction } from "../components/carousel.mjs";
 import { doFetch } from "../components/fetch.mjs";
 import { handleLoginHeader } from "../components/header.mjs";
 
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', async () => {
-    const blogs = await doFetch(
-        "GET",
-        "https://v2.api.noroff.dev/blog/posts/Swagdaddy"
-        );
-        console.log("blogsInfo", blogs);
-        blogGrid(blogs);
-        blogCarousel(blogs);
-        handleLoginHeader();
+document.addEventListener("DOMContentLoaded", async () => {
+  const blogs = await doFetch(
+    "GET",
+    "https://v2.api.noroff.dev/blog/posts/Swagdaddy"
+  );
+  console.log("blogsInfo", blogs);
+  blogGrid(blogs);
+  blogCarousel(blogs);
+  handleLoginHeader();
 });
 
 const blogCarousel = (blogs) => {
+  let latestPosts = blogs.slice(0, 3);
 
-    let latestPosts = blogs.slice(0, 3);
+  latestPosts.forEach((blog) => {
+    let container = document.getElementById("carouselContainer");
 
-    latestPosts.forEach (blog => {
-        let container = document.getElementById('carouselContainer');
+    let blogContainer = document.createElement("div");
+    blogContainer.className = "blogCarouselContainer visible";
 
-        let blogContainer = document.createElement('div');
-        blogContainer.className=('blogCarouselContainer visible');
-        
+    let image = document.createElement("img");
+    image.src = blog.media.url;
+    image.alt = blog.media.alt;
 
-        let image = document.createElement('img');
-        image.src = blog.media.url;
-        image.alt = blog.media.alt;
+    let textBox = document.createElement("div");
+    textBox.classList.add("carousel-text");
 
-        let textBox = document.createElement('div');
-        textBox.classList.add('carousel-text');
+    let title = document.createElement("h2");
+    title.textContent = blog.title;
 
-        let title = document.createElement('h2');
-        title.textContent = blog.title;
+    let button = document.createElement("button");
+    button.classList.add("carouselCta");
+    button.textContent = "Read More";
+    button.onclick = () => {
+      window.location.href = `./post/index.html?` + blog.id;
+    };
 
-        let button = document.createElement('button');
-        button.classList.add('carouselCta');
-        button.textContent = 'Read More';
-        button.onclick = () => {
-            window.location.href = `/post/index.html?`+ blog.id;
-        }
+    container.appendChild(blogContainer);
+    blogContainer.append(image, textBox);
+    textBox.append(title, button);
+  });
 
-        container.appendChild(blogContainer);
-        blogContainer.append(image, textBox);
-        textBox.append(title, button);
-    })
-
-    carouselFunction();
-}
-
+  carouselFunction();
+};
 
 const blogGrid = (blogs) => {
-    let section = document.getElementById('blogSection');
+  let section = document.getElementById("blogSection");
 
-    blogs.forEach(blog => {
-        let container = document.createElement('div');
-        container.classList.add('blogContainer');
-        container.id = 'blogContainer';
+  blogs.forEach((blog) => {
+    let container = document.createElement("div");
+    container.classList.add("blogContainer");
+    container.id = "blogContainer";
 
-        let imageContainer = document.createElement('div');
-        imageContainer.classList.add('blog-image');
+    let imageContainer = document.createElement("div");
+    imageContainer.classList.add("blog-image");
 
-        let image = document.createElement('img');
-        image.src = blog.media.url;
-        image.alt = blog.media.alt;
+    let image = document.createElement("img");
+    image.src = blog.media.url;
+    image.alt = blog.media.alt;
 
-        let titleContainer = document.createElement('div');
-        titleContainer.classList.add('blog-title');
+    let titleContainer = document.createElement("div");
+    titleContainer.classList.add("blog-title");
 
-        let title = document.createElement('h2');
-        title.classList.add('blogCardTitle');
-        title.textContent = blog.title;
+    let title = document.createElement("h2");
+    title.classList.add("blogCardTitle");
+    title.textContent = blog.title;
 
-        let buttonContainer = document.createElement('div');
-        buttonContainer.classList.add('readMoreButton');
+    let buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("readMoreButton");
 
-        let button = document.createElement('button');
-        button.textContent = 'Read More';
-        button.classList.add('read-more-a');
+    let button = document.createElement("button");
+    button.textContent = "Read More";
+    button.classList.add("read-more-a");
 
-        button.addEventListener('click', () => {
-            window.location.href = `/post/index.html?`+ blog.id;
-        });
-
-        buttonContainer.appendChild(button);
-        titleContainer.appendChild(title);
-        imageContainer.appendChild(image);
-
-        container.appendChild(imageContainer);
-        container.appendChild(titleContainer);
-        container.appendChild(buttonContainer);
-
-        section.appendChild(container);
+    button.addEventListener("click", () => {
+      window.location.href = `/post/index.html?` + blog.id;
     });
 
-    return section;
-}
+    buttonContainer.appendChild(button);
+    titleContainer.appendChild(title);
+    imageContainer.appendChild(image);
+
+    container.appendChild(imageContainer);
+    container.appendChild(titleContainer);
+    container.appendChild(buttonContainer);
+
+    section.appendChild(container);
+  });
+
+  return section;
+};
